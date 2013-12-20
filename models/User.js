11c14af0bb82bc96180d9577d2927b51718a0003
mlw214 <miller.wilt@gmail.com>
2013-12-19 20:41:51 -0500

    Project start - express files created and User.js made

---
+++ models/User.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+mongoose.connect('mongodb://localhost/lab');
+
+var userSchema = new mongoose.Schema({
+	username: { type: String, index: true },
+	salt: String,
+	password: String
+});
+
+userSchema.statics.authenticate = function (name, pass, fn) {
+	this.findOne({ username: name }, function (err, doc) {
+		if (err) return fn(err);
+		if (!docs) return fn();
+		var user = doc;
+		bcrypt.hash(pass, user.salt, function (err, hash) {
+			if (err) return fn(err);
+			if (hash == user.pass) return fn(null, user);
+			fn();
+		});
+	});
+}
+// Test for null name and pass?
+userSchema.statics.newInstance = function (name, pass, fn) {
+	var model = this;
+	this.findOne({ username: name }, function (err, doc) {
+		if (err) return fn(err);
+		if (doc) return fn();
+		var user = new model({ username: name });
+		bcrypt.genSalt(12, function (err, salt) {
+			if (err) return fn(err);
+			user.salt = salt;
+			bcrypt.hash(pass, salt, function (err, hash) {
+				if (err) return fn(err);
+				user.password = hash;
+				fn(null, user);
+			});
+		});
+	});
+}
+
+module.exports = mongoose.model('User', userSchema);
